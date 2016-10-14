3c8882abc2d18d87b4224fc051e5bc3e5e4695bd
billbunkum <william.richard.hurst@gmail.com>
2016-10-14 00:05:46 -0400

    blist-page rendering!

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,8 +1,13 @@
 import angular from 'angular';
 
+import BlistModule from '../blist/blist.module';
+// why does this need to be in 'app.module'??
+// what does 'app.module' communicate with?
+
 import appComponent from './app.component';
 
 const AppModule = angular.module('app', [
+    BlistModule.name,
 ])
     .component('app', appComponent);
 
--- client/blist/blist.module.js
+++ client/blist/blist.module.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 import 'angular-resource';
 
-import blistPageComponent from './blist-page';
+import blistPageComponent from './blist-page.component';
 
 const BlistModule = angular.module('blist', [
-    ]).config(($resourceProvider) => {
-        $resourceProvider.defaults.stripTrailingSlashes = false;
-    })
-        .component('blistPage', blistPageComponent);
+    'ngResource',
+]).config(($resourceProvider) => {
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+})
+    .component('blistPage', blistPageComponent);
 
 export default BlistModule;
