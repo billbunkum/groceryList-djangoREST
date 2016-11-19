6b30a2a48a471aa8b1f4f538ae159c291fddca9b
billbunkum <william.richard.hurst@gmail.com>
2016-11-19 14:45:06 -0500

    settings change

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).ancestor(3)
 TEMPLATE_DIR = BASE_DIR.child("templates")
 STATIC_FILE_DIR = BASE_DIR.child("static")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+#overrides automatic direct to /accounts/profile
 
 LOGIN_REDIRECT_URL = '/'
 # Quick-start development settings - unsuitable for production
