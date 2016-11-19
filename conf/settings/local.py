f347c2b8514d9712382b8a672ee10d701f5ff127
billbunkum <william.richard.hurst@gmail.com>
2016-11-19 15:48:30 -0500

    move DATABASES to local.py

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -1,3 +1,11 @@
 from .base import *
 
+# LOCAL 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DEBUG = True
